785c9cb461e92ac74a2cc6276cc24532ed426708
massamba <jeanmassamba21@gmail.com>
2018-03-28 15:35:53 +0100

    Test3

---
+++ app/src/androidTest/java/cg/beto/massamba/challenge5/MainActivityTest3.java
@@ -0,0 +1,51 @@
+package cg.beto.massamba.challenge5;
+
+import android.support.test.rule.ActivityTestRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertNotNull;
+
+public class MainActivityTest3 {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityActivityTestRule=new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    private LoginActivity mainActivity=null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity=activityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public  void testActivity(){
+
+        assertNotNull(mainActivity.findViewById(R.id.tv_email));
+        assertNotNull(mainActivity.findViewById(R.id.tv_password));
+        assertNotNull(mainActivity.findViewById(R.id.btn_login));
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        onView(withId(R.id.tv_email)).perform(typeText("gave@gmail.com")).toString();
+
+        onView(withId(R.id.tv_password)).perform(typeText("gavin"));
+
+        onView(withId(R.id.btn_login)).perform(click());
+
+        onView(withId(R.id.tv_email)).perform(clearText());
+
+        pressBack();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity=null;
+    }
+}
